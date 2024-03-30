e77faf778abc3b3a364ecd49fe26a5b2a0873a8d
Donghun <2dh2@naver.com>
2024-03-30 23:09:10 +0900

    feat. main 함수에서 try-catch문 제거

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,14 +2,8 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        try {
-            RaceController rControl = new RaceController();
-            rControl.raceStart();
-            rControl.printWinners();
-        }
-
-        catch(IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
+        RaceController rControl = new RaceController();
+        rControl.raceStart();
+        rControl.printWinners();
     }
 }
