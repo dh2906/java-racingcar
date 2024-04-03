8e3e33ea13b7c26794e20a46b6f144116a30eb6e
Donghun <2dh2@naver.com>
2024-04-03 20:54:30 +0900

    feat. 입력 예외 상황에 대한 try-catch문 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,7 +2,14 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        RaceController rControl = new RaceController();
-        rControl.raceStart();
+        try {
+            RaceController rControl = new RaceController();
+            rControl.raceStart();
+        }
+
+        catch(IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        }
     }
 }
