40b2e72a6eb275d0a2932e00421d05c3ba424f15
Donghun <2dh2@naver.com>
2025-03-24 16:10:42 +0900

    feat. 게임 결과 출력 기능 구현

--- src/main/java/racingcar/controller/OutputController.java
+++ src/main/java/racingcar/controller/OutputController.java
@@ -1,2 +1,27 @@
-package racingcar.controller;public class OutputController {
+package racingcar.controller;
+
+import racingcar.model.Car;
+import racingcar.model.Cars;
+import racingcar.view.OutputView;
+
+import java.util.List;
+
+public class OutputController {
+    private final OutputView outputView;
+
+    public OutputController() {
+        outputView = new OutputView();
+    }
+
+    public void outputProgressResultTitle() {
+        outputView.printProgressResultTitle();
+    }
+
+    public void outputProgressResult(Cars cars) {
+        outputView.printProgressResult(cars);
+    }
+
+    public void outputWinners(List<Car> winners) {
+        outputView.printWinners(winners);
+    }
 }
--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,2 +1,22 @@
-package racingcar.view;public class InputView {
+package racingcar.view;
+
+import racingcar.model.Car;
+import racingcar.model.Cars;
+
+import java.util.List;
+
+public class OutputView {
+    public void printProgressResultTitle() {
+        System.out.println("실행 결과");
+    }
+
+    public void printProgressResult(Cars cars) {
+        System.out.println(cars);
+    }
+
+    public void printWinners(List<Car> winners) {
+        String winnersString = String.join(", ", winners.stream().map(Car::getName).toList());
+
+        System.out.print("최종 우승자 : " + winnersString);
+    }
 }
