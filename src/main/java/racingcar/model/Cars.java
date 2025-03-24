a236b3b2e255c575f6ca8bd04895f14917b4d7d6
Donghun <2dh2@naver.com>
2025-03-24 16:12:22 +0900

    feat. 자동차 여러 개 담는 클래스 생성 및 구현

--- src/main/java/racingcar/model/Cars.java
+++ src/main/java/racingcar/model/Cars.java
@@ -1,2 +1,44 @@
-package racingcar.model;public class Cars {
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+public class Cars {
+    private final List<Car> carList;
+    private final Random random;
+
+    public Cars() {
+        carList = new ArrayList<>();
+        random = new Random();
+    }
+
+    public void join(Car car) {
+        carList.add(car);
+    }
+
+    public void tryToGo() {
+        carList.forEach((car) ->
+                car.move(random.nextInt(10)));
+    }
+
+    public List<Car> getCarList() {
+        return carList;
+    }
+
+    public int getMaxPos() {
+        return carList.stream().mapToInt(Car::getPos).max().getAsInt();
+    }
+
+    public List<Car> getWinners() {
+        return carList.stream().filter(car ->
+            car.getPos() == getMaxPos()
+        ).collect(Collectors.toList());
+    }
+
+    @Override
+    public String toString() {
+        return String.join("\n", carList.stream().map(Car::toString).toList()) + "\n";
+    }
 }
