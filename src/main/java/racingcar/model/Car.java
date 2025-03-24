9e6dc535ff94df5d5273023b02a7cbe580059049
Donghun <2dh2@naver.com>
2025-03-24 16:11:32 +0900

    feat. 자동차 클래스 구현 구체화

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -4,5 +4,26 @@ public class Car {
     String name;
     int pos;
 
+    public Car(String name) {
+        this.name = name;
+        this.pos = 0;
+    }
 
+    public void move(int num) {
+        if (num >= 4)
+            pos++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPos() {
+        return pos;
+    }
+
+    @Override
+    public String toString() {
+        return name + " : " + "-".repeat(getPos());
+    }
 }
