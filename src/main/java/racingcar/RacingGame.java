1bd4cb623c3eb8d70bf0c51ac85d70f56d4b8fd8
Donghun <2dh2@naver.com>
2025-03-24 16:13:11 +0900

    feat. 레이싱 게임 관리 클래스 생성

--- src/main/java/racingcar/RacingGame.java
+++ src/main/java/racingcar/RacingGame.java
@@ -1,2 +1,42 @@
-package racingcar;public class RacingGame {
+package racingcar;
+
+import racingcar.controller.InputController;
+import racingcar.controller.OutputController;
+import racingcar.model.Car;
+import racingcar.model.Cars;
+
+import java.util.List;
+import java.util.Random;
+
+public class RacingGame {
+    private final Cars cars;
+    private final InputController inputController;
+    private final OutputController outputController;
+
+    public RacingGame() {
+        cars = new Cars();
+        inputController = new InputController();
+        outputController = new OutputController();
+    }
+
+    public void run() {
+        List<String> nameList = inputController.inputCarNames();
+        int tryTimes = inputController.inputTryTimes();
+
+        nameList.forEach((name) -> {
+            cars.join(new Car(name));
+        });
+
+        outputController.outputProgressResultTitle();
+
+        for (int i = 0; i < tryTimes; i++) {
+            cars.tryToGo();
+            outputController.outputProgressResult(cars);
+        }
+
+        List<Car> winners = cars.getWinners();
+
+        outputController.outputWinners(winners);
+
+    }
 }
